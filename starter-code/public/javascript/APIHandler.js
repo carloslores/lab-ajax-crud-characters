adb095ba70bcea9ee4163f9181242fdda49c0169
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos.local>
2019-03-08 08:15:25 +0100

    not dont

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,32 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
 
-  getFullList () {
+    getFullList() {
 
-  }
+        axios.post('http://localhost:8000/characters', character)
+            .then(response => {
+                const info = `<li>${response.data.name} (id ${response.data.id}) <br>${response.data.occupation}</li>`
+                document.getElementById('characters-list').innerHTML += info;
+            })
+            .catch(e => console.log(`¡Ops! Error: ${e}`))
 
-  getOneRegister () {
+    }
 
-  }
+    getOneRegister() {
 
-  createOneRegister () {
+    }
 
-  }
+    createOneRegister() {
 
-  updateOneRegister () {
+    }
 
-  }
+    updateOneRegister() {
 
-  deleteOneRegister () {
+    }
 
-  }
+    deleteOneRegister() {
+
+    }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,44 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+    document.getElementById('fetch-all').onclick = (e) => {
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
+        charactersAPI.getFullList()
+
+        .then(response => {
+                let info = response.data
+                    //(id ${response.data.id}) <br>${response.data.occupation}</li>`)
+                console.log(info)
+            })
+            .catch(e => console.log(`¡Ops! Error: ${e}`))
+
+    }
+
+    document.getElementById('fetch-one').onclick = function() {
+
+        e.preventDefault()
+
+
+        axios.get('http://localhost:8000/characters', charactersAPI)
+            .then(response => {
+                let one = response.data.array.forEach(element => {
+
+                });
+                //(id ${response.data.id}) <br>${response.data.occupation}</li>`)
+                console.log(one)
+            })
+            .catch(e => console.log(`¡Ops! Error: ${e}`))
+    }
+
+    document.getElementById('delete-one').onclick = function() {
+
+    }
+
+    document.getElementById('edit-character-form').onsubmit = function() {
+
+    }
+
+    document.getElementById('new-character-form').onsubmit = function() {
+
+    }
 })
